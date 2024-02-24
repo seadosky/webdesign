f0ab0b866e27923f30edf3f8d5ea998b239ae6fc
seadosky <157003372+seadosky@users.noreply.github.com>
2024-02-24 23:22:53 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -21,4 +21,22 @@ $(document).ready(function(){
         $('.header').removeClass('menu_over')
     })
 
+    /*swiper */
+    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .prev',  
+        },
+
+    });
+    swiper.autoplay.stop();  /* 일시정지 기능 */
+    swiper.autoplay.start();  /* 재생 기능 */
 })
