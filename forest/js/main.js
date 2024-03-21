b7f46e7fdf1e08cdcc84a4a1b70898131825ae77
seadosky <157003372+seadosky@users.noreply.github.com>
2024-03-21 13:26:05 +0900

    .

--- forest/js/main.js
+++ forest/js/main.js
@@ -31,18 +31,17 @@ $(document).ready(function(){
 	effect: "fade", /* fade 효과 */
 
 	autoplay: {  /* 팝업 자동 실행 */
-		delay: 2500,
+		delay: 5000,
 		disableOnInteraction: true,
 	},
 
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
 	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		el: '.visual .paging', /* 해당 요소의 class명 */
 		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
 		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		    return '<span class="page' + (index + 1) + ' '+className+'"><span></span></span>';
 		},
 	},
 
