fa0c18af31380cd5c4b51cce35c1cf5375816607
seadosky <157003372+seadosky@users.noreply.github.com>
2024-03-29 15:54:01 +0900

    .

--- webtest/test01_02/js/main.js
+++ webtest/test01_02/js/main.js
@@ -9,17 +9,23 @@ $(document).ready(function(){
     $('.header .gnb .depth1 > li:last-child > ul > li:last-child > a').on('focusout', function(){
         $('.header .gnb .depth1 > li').removeClass('on')
     })
+            
+    let idx = 1
+    setInterval(function(){
+        if(idx < 3){
+            idx++
+        }else{
+            idx = 1
+        }
+        $('.visual ul li').removeClass('active')
+        $('.visual ul li').eq(idx-1).addClass('active')
+    })
 
+    $(doocument).ready(function(){
+        $('.header .gnb .depth1 > li').on('mouseenter focusin', function(){
+            $('header .gnb .depth1 > li').removeClass('on')
+            $(this).addClass('on')
+        })
+    })
 })//document.ready
 
-
-let idx = 1
-setInterval(function(){
-    if(idx < 3){
-        idx++
-    }else{
-        idx = 1
-    }
-    $('.visual ul li').removeClass('active')
-    $('.visual ul li').eq(idx-1).addClass('active')
-})
